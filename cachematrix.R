d5c48c7e87411d77da41e91e3c89a6568c71a1f6
Mukundh0007 <93877999+Mukundh0007@users.noreply.github.com>
2021-11-07 22:58:04 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#makeCacheMatrix()<==creates a special "matrix" object that caches its inverse i.e. duplicates it in a global environment
+#cacheSolve: computes the inverse of the special "matrix" returned by makeCacheMatrix and if the inverse has already been calculated, then the 
+#            cachesolve retrieves the inverse from the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## functions to do-makeCacheMatrix,cacheSolve
+
+#Making the Cache Matrix function
+makeCacheMatrix<-function(m=matrix()){
+  invrs<-NULL
+  set<-function(n){
+    m<<-n
+    invrs<<-NULL
+  }
+  get<-function(){m}
+  setInv<-function(inverse){invrs<<-inverse}
+  getInv<-function(){invrs}
+  list(set=set,get=get,setInv=setInv,getInv=getInv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Solving the Cache Matrix
+cacheSolve<-function(m,...){
+  invrs<-m$getInv()
+  if(!is.null(invrs)){
+    message('Getting cache data')
+    return(invrs)
+  }
+  matrx<-m$get()
+  invrs<-solve(matrx,...)    #solve() returns the matrix inverse as output
+  m$setInv(invrs)
+  invrs   ## Returns a matrix that is the inverse of 'm'
 }
